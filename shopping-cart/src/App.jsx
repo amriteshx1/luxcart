6bdc7362f3f8825a942a1ed5a9ac1637836ea890
amriteshx1 <amriteshx18@gmail.com>
2025-03-26 20:03:31 +0530

    refactor: remove nested folders and set up routing with react-router-dom

--- shopping-cart/src/App.jsx
+++
@@ -1,5 +0,0 @@
-function App (){
-  return <h1>HIIIII</h1>
-}
-
-export default App
---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+
+function App (){
+  return(
+    <h1>Hiiii, let's start the fun!</h1>
+  ) 
+}
+
+export default App
