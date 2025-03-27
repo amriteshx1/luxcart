8d7b291d78a2c831da75a448b6a34ec8715fffaf
amriteshx1 <amriteshx18@gmail.com>
2025-03-27 19:33:15 +0530

    Completed Home with full styling and assets ✅

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,15 @@
+import HomeImg from '../../assets/HomeImage.jpg'
+import styles from './Home.module.css';
 function Home(){
-    return <h1>Hii, I am Homepage.</h1>
+    return(
+        <div className={styles.mainCnt}>
+            <div className={styles.cnt1}>
+                <p>Discover the perfect blend of elegance, comfort, and trend with our exclusive fashion collections.</p>
+            </div>
+            <div className={styles.cnt2}>
+                <img src={HomeImg} alt="homepage-mainImage" className={styles.homeImg}/>
+            </div>
+        </div>
+    )
 }
 export default Home;
