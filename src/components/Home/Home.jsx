c80c2405a64818262250870fe51a863de81d96b5
amriteshx1 <amriteshx18@gmail.com>
2025-03-29 18:19:20 +0530

    Updated Home with Shop Now button & some final layout touches

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,19 @@
+import { useNavigate} from "react-router-dom";
 import HomeImg from '../../assets/HomeImage.jpg'
 import styles from './Home.module.css';
 function Home(){
+    const navigate = useNavigate();
+
+    function handleClick(event){
+        event.preventDefault();
+        navigate("/shop");
+    }
+
     return(
         <div className={styles.mainCnt}>
             <div className={styles.cnt1}>
                 <p>Discover the perfect blend of elegance, comfort, and trend with our exclusive fashion collections.</p>
+                <button onClick={(event) => handleClick(event)}>Shop Now</button>
             </div>
             <div className={styles.cnt2}>
                 <img src={HomeImg} alt="homepage-mainImage" className={styles.homeImg}/>
